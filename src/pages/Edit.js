4c97dbd75b6195af73e0912b6b752b7365e8cc21
Zayne <star1582@naver.com>
2022-11-16 22:58:53 +0900

    Myheader & MyButton 구현

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,19 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const id = searchParams.get("id");
+  console.log("id :", id);
+  const mode = searchParams.get("mode");
+  console.log("mode :", mode);
   return (
     <div>
       <h1>Edit</h1>
       <p>이곳은 일기 수정 페이지 입니다.</p>
+      <button onClick={() => setSearchParams({ who: "lee" })}>QS 바꾸기</button>
+      <button onClick={() => navigate("/home")}>Home으로 가기</button>
+      <button onClick={() => navigate(-1)}>뒤로가기</button>
     </div>
   );
 };
