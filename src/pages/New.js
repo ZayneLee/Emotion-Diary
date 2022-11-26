126e1df06c34c2f4d4d78daf0dfaf74c44659c6d
Zayne <star1582@naver.com>
2022-11-26 21:12:08 +0900

    New 화면 개발

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,8 +1,36 @@
+import MyHeader from "./../components/MyHeader";
+import MyButton from "./../components/MyButton";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+const getStringDate = (date) => {
+  return date.toISOString().slice(0, 10);
+};
 const New = () => {
+  const navigate = useNavigate();
+  const [date, setDate] = useState(getStringDate(new Date()));
+
   return (
     <div>
-      <h1>New</h1>
-      <p>이곳은 일기 작성 페이지 입니다.</p>
+      <MyHeader
+        headText={"새 일기쓰기"}
+        leftChild={
+          <MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />
+        }
+      />
+      <div>
+        <section>
+          <h4>오늘은 언제인가요?</h4>
+          <div className="input-box">
+            <input
+              className="input-date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              type="date"
+            />
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
